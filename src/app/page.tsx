803f165dd9cfd6e273da915808416e40f93a132f
Harshit <kashyapharshit50@gmail.com>
2024-09-21 20:42:30 +0530

    thora-complex-samosa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,25 @@ import { useEffect, useState } from "react";
 import Post from "@/components/Post";
 
 export default function Home() {
+  const [query, setQuery] = useState("")
   const [postUrl, setPostUrl] = useState("https://jsonplaceholder.typicode.com/posts")
   const [postArr, setPostArr] = useState([])
   useEffect(() => {
     axios.get(postUrl).then(response => setPostArr(response.data))
   }, [])
+  useEffect(() => {
+    if(query.length === 0){
+      
+      axios.get(postUrl).then(response => setPostArr(response.data))
+    }else{
+
+      axios.get(postUrl, {
+        params: {
+          id: query
+        }
+      }).then(response => setPostArr(response.data))
+    }
+  }, [query])
   useEffect(() => {
     console.log(postArr)
   }, postArr)
@@ -19,12 +33,17 @@ export default function Home() {
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand">Navbar</a>
         <form className="form-inline flex">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <input onChange={
+            (event) => {
+              setQuery(event.target.value)
+              console.log(event.target.value)
+            }
+          } className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
       </nav>
       {postArr.map((el, ind) => (
-        <Post key={ind} id={ind} title={el.title} body={el.body} />
+        <Post key={ind} id={el.id} title={el.title} body={el.body} />
 
       ))}
     </div>
